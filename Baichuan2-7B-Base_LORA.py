55da84db3e8d013911b5fe4c91a8fcc3ce9bcdf9
WeiyaoLi <nolanwyl@gmail.com>
2023-11-09 15:21:47 -0500

    pip

--- Baichuan2-7B-Base_LORA.py
+++ Baichuan2-7B-Base_LORA.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-pip install datasets transformers torch tqdm pandas huggingface_hub
-pip install protobuf transformers==4.30.2 cpm_kernels torch>=2.0 gradio mdtex2html sentencepiece accelerate
-pip install torch torchvision torchaudio
-pip install transformers
-pip install loguru
-pip install datasets
-pip install peft
-pip install bitsandbytes
-pip install tensorboard
-pip install sentencepiece
-pip install accelerate -U
-pip install transformers==4.30.2 peft==0.4.0
-pip install sentencepiece
-pip install accelerate
-pip install torch
-pip install peft
-pip install datasets
-pip install bitsandbytes
-pip install --upgrade peft
-
-# In[1]:
-
-
 import os
 import shutil
 
@@ -36,41 +11,18 @@ if os.path.exists(jsonl_path):
 if os.path.exists(save_path):
     shutil.rmtree(save_path)
 
-import os
-
 directory = "../data"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-
-# In[2]:
-
-
-get_ipython().system('pip install datasets transformers torch tqdm pandas huggingface_hub')
-get_ipython().system('pip install protobuf transformers==4.30.2 cpm_kernels torch>=2.0 gradio mdtex2html sentencepiece accelerate')
-
-
-# In[3]:
-
-
 from datasets import load_dataset
 import datasets
-
-
-# 1. TFNS
-
-# In[4]:
-
 
 dic = {
     0:"negative",
     1:'positive',
     2:'neutral',
 }
-
-
-# In[5]:
-
 
 social_media_dataset = load_dataset('zeroshot/twitter-financial-news-sentiment')
 social_media_dataset = social_media_dataset['train']
@@ -79,35 +31,17 @@ social_media_dataset['label'] = social_media_dataset['label'].apply(lambda x:dic
 social_media_dataset['instruction'] = 'What is the sentiment of this tweet? Please choose an answer from {negative/neutral/positive}.'
 social_media_dataset.columns = ['input', 'output', 'instruction']
 social_media_dataset = datasets.Dataset.from_pandas(social_media_dataset)
-social_media_dataset
 
-
-# In[6]:
-
+print(social_media_dataset)
 
 tmp_dataset = datasets.concatenate_datasets([social_media_dataset]*2)
 train_dataset = tmp_dataset
 print(tmp_dataset.num_rows)
 
-
-# In[7]:
-
-
 all_dataset = train_dataset.shuffle(seed = 42)
-all_dataset.shape
-
-
-# Make Dataset
-
-# In[8]:
-
 
 import json
 from tqdm.notebook import tqdm
-
-
-# In[9]:
-
 
 def format_example(example: dict) -> dict:
     context = f"Instruction: {example['instruction']}\n"
@@ -117,10 +51,6 @@ def format_example(example: dict) -> dict:
     target = example["output"]
     return {"context": context, "target": target}
 
-
-# In[10]:
-
-
 data_list = []
 for item in all_dataset.to_pandas().itertuples():
     tmp = {}
@@ -129,30 +59,13 @@ for item in all_dataset.to_pandas().itertuples():
     tmp["output"] = item.output
     data_list.append(tmp)
 
-
-# In[11]:
-
-
 with open("../data/dataset_new.jsonl", 'w') as f:
     for example in tqdm(data_list, desc="formatting.."):
         f.write(json.dumps(format_example(example)) + '\n')
 
 
-# Tokenize
-
-# In[11]:
-
-
-
-
-
-# In[12]:
-
-
-import json
 from tqdm.notebook import tqdm
 
-import datasets
 from transformers import AutoTokenizer, AutoConfig
 
 model_name = "baichuan-inc/Baichuan2-7B-Base"
@@ -160,10 +73,6 @@ jsonl_path = "../data/dataset_new.jsonl"  # updated path
 save_path = '../data/dataset_new'  # updated path
 max_seq_length = 512
 skip_overlength = True
-
-
-# In[13]:
-
 
 def preprocess(tokenizer, config, example, max_seq_length):
     prompt = example["context"]
@@ -191,19 +100,10 @@ def read_jsonl(path, max_seq_length, skip_overlength=False):
             feature["input_ids"] = feature["input_ids"][:max_seq_length]
             yield feature
 
-
-# 
-
-# In[14]:
-
-
 dataset = datasets.Dataset.from_generator(
     lambda: read_jsonl(jsonl_path, max_seq_length, skip_overlength)
     )
 dataset.save_to_disk(save_path)
-
-
-# In[15]:
 
 
 from datasets import load_from_disk
@@ -215,38 +115,9 @@ num_samples = loaded_dataset.num_rows
 print(f'Number of samples in the dataset: {num_samples}')
 
 
-# In[15]:
-
-
-
-
-
-# The start of LORA:
-
-# In[16]:
-
-
-get_ipython().system('pip install torch torchvision torchaudio')
-get_ipython().system('pip install transformers')
-get_ipython().system('pip install loguru')
-get_ipython().system('pip install datasets')
-get_ipython().system('pip install peft')
-get_ipython().system('pip install bitsandbytes')
-get_ipython().system('pip install tensorboard')
-get_ipython().system('pip install sentencepiece')
-
-
-# In[17]:
-
-
-# only for WSL
-import os
+# The Start of LORA
 os.environ["PATH"] = f"{os.environ['PATH']}:/usr/local/cuda/bin"
 os.environ['LD_LIBRARY_PATH'] = "/usr/lib/wsl/lib:/usr/local/cuda/lib64"
-
-
-# In[18]:
-
 
 from typing import List, Dict, Optional
 
@@ -271,16 +142,6 @@ from peft import (
 )
 from peft.utils import TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING
 
-
-# In[19]:
-
-
-get_ipython().system('pip install accelerate -U')
-
-
-# In[20]:
-
-
 training_args = TrainingArguments(
         output_dir='./finetuned_model',    # saved model path
         logging_steps = 500,
@@ -298,30 +159,17 @@ training_args = TrainingArguments(
         load_best_model_at_end = True,
         evaluation_strategy="steps",
         remove_unused_columns=False,
-
     )
 
-
-# In[21]:
-
-
-# Quantization
+ # Quantization
 q_config = BitsAndBytesConfig(load_in_4bit=True,
-                               bnb_4bit_quant_type='nf4',
-                               bnb_4bit_use_double_quant=True,
-                               bnb_4bit_compute_dtype=torch.float16
-                               )
-
-
-# In[22]:
-
+                                bnb_4bit_quant_type='nf4',
+                                bnb_4bit_use_double_quant=True,
+                                bnb_4bit_compute_dtype=torch.float16
+                                )
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
-
-# In[23]:
-
 
 # Load tokenizer & model
 model_name = "baichuan-inc/Baichuan2-7B-Base"
@@ -333,10 +181,6 @@ model = AutoModelForCausalLM.from_pretrained(
         device='cuda'
     )
 model = prepare_model_for_int8_training(model, use_gradient_checkpointing=True)
-
-
-# In[24]:
-
 
 def print_trainable_parameters(model):
     """
@@ -351,10 +195,6 @@ def print_trainable_parameters(model):
     print(
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
-
-
-# In[25]:
-
 
 # LoRA
 TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING['baichuan-inc/Baichuan2-7B-Base'] = ["W_pack", "o_proj"]
@@ -372,9 +212,6 @@ model = get_peft_model(model, lora_config)
 print_trainable_parameters(model)
 
 
-# In[26]:
-
-
 resume_from_checkpoint = None
 if resume_from_checkpoint is not None:
     checkpoint_name = os.path.join(resume_from_checkpoint, 'pytorch_model.bin')
@@ -390,23 +227,11 @@ if resume_from_checkpoint is not None:
     else:
         logger.info(f'Checkpoint {checkpoint_name} not found')
 
-
-# In[27]:
-
-
 model.print_trainable_parameters()
-
-
-# In[28]:
-
 
 # load data
 dataset = datasets.load_from_disk("../data/dataset_new")
 dataset = dataset.train_test_split(0.2, shuffle=True, seed = 42)
-
-
-# In[29]:
-
 
 class ModifiedTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
@@ -455,16 +280,8 @@ def data_collator(features: list) -> dict:
         "labels": labels,
     }
 
-
-# In[30]:
-
-
 from torch.utils.tensorboard import SummaryWriter
 from transformers.integrations import TensorBoardCallback
-
-
-# In[31]:
-
 
 # Train
 writer = SummaryWriter()
@@ -481,25 +298,6 @@ writer.close()
 # save model
 model.save_pretrained(training_args.output_dir)
 
-
-# In[32]:
-
-
-get_ipython().system('zip -r /content/saved_model.zip /content/{training_args.output_dir}')
-
-
-# In[33]:
-
-
-from google.colab import files
-files.download('/content/saved_model.zip')
-
-
-# In[34]:
-
-
-import os
-
 def get_folder_size(folder_path):
     total_size = 0
     for dirpath, _, filenames in os.walk(folder_path):
@@ -512,31 +310,8 @@ model_size = get_folder_size(training_args.output_dir)
 print(f"Model size: {model_size} MB")
 
 
-# Inference in benchmarks
-
-# In[35]:
-
-
-get_ipython().system('pip install transformers==4.30.2 peft==0.4.0')
-get_ipython().system('pip install sentencepiece')
-get_ipython().system('pip install accelerate')
-get_ipython().system('pip install torch')
-get_ipython().system('pip install peft')
-get_ipython().system('pip install datasets')
-get_ipython().system('pip install bitsandbytes')
-
-
-# In[36]:
-
-
-get_ipython().system('git clone https://github.com/AI4Finance-Foundation/FinNLP.git')
-
 import sys
-sys.path.append('/content/FinNLP/')
-
-
-# In[37]:
-
+sys.path.append('../content/FinNLP/')
 
 from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
 
@@ -548,38 +323,6 @@ from finnlp.benchmarks.fiqa import test_fiqa , add_instructions
 from finnlp.benchmarks.tfns import test_tfns
 from finnlp.benchmarks.nwgi import test_nwgi
 
-
-# In[38]:
-
-
-get_ipython().system('pip install --upgrade peft')
-
-
-# In[39]:
-
-
-from google.colab import drive
-drive.mount('/content/drive')
-
-
-# In[40]:
-
-
-import os
-
-# Define the path you want to check
-path_to_check = "/content/drive/My Drive/finGPTresearch/content"
-
-# Check if the specified path exists
-if os.path.exists(path_to_check):
-    print("Path exists.")
-else:
-    print("Path does not exist.")
-
-
-# In[45]:
-
-
 base_model = "baichuan-inc/Baichuan2-7B-Base"
 peft_model = training_args.output_dir
 
@@ -590,107 +333,20 @@ model = PeftModel.from_pretrained(model, peft_model)
 
 model = model.eval()
 
-
-# In[46]:
-
-
-# base_model = "baichuan-inc/Baichuan2-7B-Base"
-# peft_model = "/content/drive/My Drive/finGPTresearch/content/finetuned_model"
-
-# tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
-# model = AutoModel.from_pretrained(base_model, trust_remote_code=True, load_in_8bit=True, device_map="auto")
-
-# model = PeftModel.from_pretrained(model, peft_model)
-
-# model = model.eval()
-
-
-# In[53]:
-
-
 batch_size = 16
-
-
-# In[54]:
-
 
 # TFNS
 res = test_tfns(model, tokenizer, batch_size = batch_size)
-
-
-# In[ ]:
-
 
 # FPB
 res = test_fpb(model, tokenizer, batch_size = batch_size)
 
 
-# In[ ]:
-
-
 # FiQA
 res = test_fiqa(model, tokenizer, prompt_fun = add_instructions, batch_size = batch_size)
 
-
-# In[ ]:
-
-
 # NWGI
 res = test_nwgi(model, tokenizer, batch_size = batch_size)
-
-
-# Conclusion:
-# 
-# ***TFNS:***
-# Preivous:
-# Acc: 0.876465661641541. F1 macro: 0.8422150651552788. F1 micro: 0.876465661641541. F1 weighted (BloombergGPT): 0.8760116165894634.
-# 
-# Current:
-# Acc: 0.8618090452261307. F1 macro: 0.828965375193647. F1 micro: 0.8618090452261307. F1 weighted (BloombergGPT): 0.8632472452505952.
-# 
-# 
-# 
-# ----
-# ***FRB:***
-# Preivous:
-# Acc: 0.8531353135313532. F1 macro: 0.8355311905054061. F1 micro: 0.8531353135313532. F1 weighted (BloombergGPT): 0.8518766219402919.
-# 
-# Current:
-# Acc: 0.7970297029702971. F1 macro: 0.7575108820595529. F1 micro: 0.7970297029702971. F1 weighted (BloombergGPT): 0.7781642224313978.
-# 
-# ----
-# ***FiQA:***
-# Preivous:
-# Acc: 0.8327272727272728. F1 macro: 0.7457078313253013. F1 micro: 0.8327272727272728. F1 weighted (BloombergGPT): 0.8475613362541076.
-# 
-# Current:
-# Acc: 0.6254545454545455. F1 macro: 0.5741047534089461. F1 micro: 0.6254545454545455. F1 weighted (BloombergGPT): 0.6876670875957228.
-# 
-# 
-# ----
-# ***NWGI:***
-# Preivous:
-# Acc: 0.6375092661230541. F1 macro: 0.6449413175330677. F1 micro: 0.6375092661230541. F1 weighted (BloombergGPT): 0.6368355381269861.
-# 
-# Current:
-# Acc: 0.5688164072152212. F1 macro: 0.569744027675689. F1 micro: 0.5688164072152212. F1 weighted (BloombergGPT): 0.5614102592371769.
-
-# In[ ]:
-
-
-
-
-
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
