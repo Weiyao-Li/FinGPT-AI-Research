6ef7f7f23ee01ffd4079ce0eb3d06f51bfc2d7d6
WeiyaoLi <nolanwyl@gmail.com>
2023-11-12 20:18:39 -0500

    debug inference

--- FinGPT_Benchmark/inference.py
+++ FinGPT_Benchmark/inference.py
@@ -20,12 +20,12 @@ def load_model(base_model, peft_model, from_remote=False):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
     tokenizer.padding_side = "left"
-    if base_model == 'qwen':
-        tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
-        tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
-    if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
-        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-        model.resize_token_embeddings(len(tokenizer))
+    # if base_model == 'qwen':
+    #     tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
+    #     tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
+    # if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
+    #     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    #     model.resize_token_embeddings(len(tokenizer))
 
     model = PeftModel.from_pretrained(model, peft_model)
     model = model.eval()
