70a962eafb9c62f8670d583530467554cfca57a9
WeiyaoLi <nolanwyl@gmail.com>
2023-11-12 20:28:04 -0500

    debug inference

--- FinGPT_Benchmark/inference.py
+++ FinGPT_Benchmark/inference.py
@@ -20,12 +20,12 @@ def load_model(base_model, peft_model, from_remote=False):
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
     tokenizer.padding_side = "left"
-    # if base_model == 'qwen':
-    #     tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
-    #     tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
-    # if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
-    #     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    #     model.resize_token_embeddings(len(tokenizer))
+    if base_model == 'qwen':
+        tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
+        tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
+    if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        model.resize_token_embeddings(len(tokenizer))
 
     model = PeftModel.from_pretrained(model, peft_model)
     model = model.eval()
@@ -34,7 +34,7 @@ def load_model(base_model, peft_model, from_remote=False):
 
 if __name__ == "__main__":
     base_model = 'baichuan-inc/Baichuan2-7B-Base'
-    peft_model = 'finetuned_models/sentiment-baichuan-7b-20epoch-8batch_202311130422#'
+    peft_model = 'finetuned_models/sentiment-baichuan-7b-20epoch-8batch_202311130422'
     FROM_REMOTE = False
 
     model, tokenizer = load_model(base_model, peft_model, FROM_REMOTE)
